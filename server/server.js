7dfd5ba7726a1f8819a5c9f29a54981ed4ea5728
komalahuja0 <komalahuja363@gmail.com>
2026-06-14 21:00:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const weatherRoutes=require("./Routes/weatherRoutes");
+const weatherRoutes=require("./Routes/WeatherRoutes");
     
 
 const app = express();
